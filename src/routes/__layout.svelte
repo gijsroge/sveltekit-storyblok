1725fc13c2a1ea081363c735f11034d6f4dfce75
gijs.roge <gijs.roge@intracto.com>
2022-03-09 16:35:26 +0100

    added prefetch

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,11 +3,11 @@
 </script>
 
 <header class="max-w-screen-2xl mx-auto w-full py-8 flex justify-between">
-	<a rel="prefetch" href="/nl-be">Home</a>
+	<a sveltekit:prefetch href="/nl-be">Home</a>
 
 	<nav>
 		<ul class="flex space-x-3">
-			<li><a rel="prefetch" href="/nl-be/hobby/blog-1">blog</a></li>
+			<li><a sveltekit:prefetch href="/nl-be/hobby/blog-1">blog</a></li>
 		</ul>
 	</nav>
 </header>
--- src/storyblok/components/title.svelte
+++ src/storyblok/components/title.svelte
@@ -5,9 +5,9 @@
 </script>
 
 {#if block.tag === 'h1'}
-	<h1 class="{className}">{block.title}</h1>
+	<h1 class="text-4xl {className}">{block.title}</h1>
 {:else if block.tag === 'h2'}
-	<h2 class="{className}">{block.title}</h2>
+	<h2 class="text-3xl {className}">{block.title}</h2>
 {:else}
-	<h3 class="{className}">{block.title}</h3>
+	<h3 class="text-2xl {className}">{block.title}</h3>
 {/if}
