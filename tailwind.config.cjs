fad458021ef8578059ae7ad240069ecc442c8a9b
gijs.roge <gijs.roge@intracto.com>
2022-03-11 14:25:30 +0100

    remove prestyled forms

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
 	theme: {
 		extend: {}
 	},
-	plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+	plugins: [require('@tailwindcss/typography')],
 	safelist: [
 		{
 			pattern: /(p|py|mb|bg)-(2|4|6|8|10|black|blue-500)|max-w-(4|6|8)xl/
