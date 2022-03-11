00502e920a8d76031051687c7df6c6eeb9b3bd99
gijs.roge <gijs.roge@intracto.com>
2022-03-11 14:24:33 +0100

    add link component

---
+++ src/components/link.svelte
@@ -0,0 +1,6 @@
+<script>
+	import { page } from '$app/stores';
+	export let href;
+</script>
+
+<a {href} {...$$restProps} class:font-bold={$page.url.pathname === href}><slot /></a>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -8,23 +8,25 @@
 	import '@/assets/css/app.css';
 	import Footer from '$components/footer.svelte';
 	import ThemeSwitch from '@/components/themeSwitch.svelte';
+	import { page } from '$app/stores';
 	import { theme } from '$stores/theme';
+	import Link from '$components/link.svelte';
 </script>
 
 <svelte:head>
 	<meta name="color-scheme" content={$theme == 'system' ? 'light dark' : $theme} />
 </svelte:head>
 
-<header class="wrapper w-full py-8 flex justify-between">
-	<a sveltekit:prefetch href="/nl-be">Home</a>
-
-	<div class="flex space-x-6 items-center">
-		<ThemeSwitch />
-
-		<ul class="flex space-x-3">
-			<li><a sveltekit:prefetch href="/nl-be/hobby/blog-1">blog</a></li>
-		</ul>
-	</div>
+<header class="wrapper w-full py-8 flex items-center justify-between">
+	<ul class="flex space-x-3">
+		<li>
+			<Link sveltekit:prefetch href="/nl-be">Home</Link>
+		</li>
+		<li>
+			<Link sveltekit:prefetch href="/nl-be/hobby/blog-1">Blog</Link>
+		</li>
+	</ul>
+	<ThemeSwitch />
 </header>
 <slot />
 <footer class="bg-black p-4 text-white mt-auto"><Footer /></footer>
